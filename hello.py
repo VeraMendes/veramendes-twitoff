ad232e19827f036548d8bfe294bf53ba742401a2
veramendes <veramendes@users.noreply.github.com>
2019-11-14 12:52:30 -0800

    changes

---
+++ hello.py
@@ -0,0 +1,22 @@
+"""Minimal Flask app"""
+
+from flask import Flask, render_template
+
+# make the application
+app = Flask(__name__)
+
+# make the route
+@app.route("/")
+
+# now we define a function
+def hello():
+    return render_template('home.html')
+
+# creating another route
+@app.route("/about")
+
+def preds():
+    return render_template('about.html')
+
+
+
